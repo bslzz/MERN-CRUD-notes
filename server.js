0fc89bf48e076f5e8acb1ba8f2873b74e47c7a3a
bslzz <bishalkoid@gmail.com>
2020-08-16 11:13:59 +0300

    initial commit after installing necessary files and setting up initial test route in server

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+require('dotenv').config();
+const cors = require('cors');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.json('Hello from express');
+});
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => console.log(`Server running at ${PORT}`));
