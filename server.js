e18bf5e9d9cf3bc1ad3cfe73721267fdd1cea9db
bslzz <bishalkoid@gmail.com>
2020-08-16 11:44:02 +0300

    connected to mongodb

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
+require('./helpers/initMongoose');
 
 const app = express();
 app.use(express.json());
