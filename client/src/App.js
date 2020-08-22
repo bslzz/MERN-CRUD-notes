eaa78dfc66d3b826c78973230eddbe4d82b9966c
bslzz <bishalkoid@gmail.com>
2020-08-22 21:23:20 +0300

    user token status created and saved to localStorage if the user is logged in

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Login from './components/Login';
 import Notes from './components/Notes';
 import Register from './components/Register';
+import axios from 'axios';
 
 const App = () => {
   const [isLogin, setIsLogin] = useState(false);
+
+  useEffect(() => {
+    const checkLogin = async () => {
+      const token = localStorage.getItem('tokenStore');
+      if (token) {
+        const verified = await axios.get('/users/verify', {
+          headers: { Authorization: token },
+        });
+        console.log(verified);
+        setIsLogin(verified.data);
+        if (verified.data === false) return localStorage.clear();
+      } else {
+        setIsLogin(false);
+      }
+    };
+    checkLogin();
+  }, []);
+
   return (
     <>
       {isLogin ? <Notes /> : <Login setIsLogin={setIsLogin} />}
