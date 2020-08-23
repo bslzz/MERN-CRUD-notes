0239b621f2ff9d3f6e4cde1ff956b7dde2f1e3d1
bslzz <bishalkoid@gmail.com>
2020-08-23 13:01:03 +0300

    initial deploy

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 require('dotenv').config();
 require('./helpers/initMongoose');
+const path = require('path');
 
 const app = express();
 app.use(express.json());
@@ -9,6 +10,14 @@ app.use(express.json());
 app.use('/users', require('./routes/userRouter'));
 app.use('/notes', require('./routes/noteRouter'));
 
+//Deploy
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 //Listen server
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running at ${PORT}`));
